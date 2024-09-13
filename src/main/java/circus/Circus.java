b4844d6d2fce62144c26462f7bc81651ef53b043
coraleaf0602 <zhangco@tcd.ie>
2024-09-13 17:19:58 +0800

    Revert "Fix imports in Circus"
    This reverts commit 6383db0398f310bcfd5905357090e345fffeb2dd.

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -3,16 +3,10 @@ package circus;
 import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
-import circus.animal.Elephant;
 import circus.animal.Tiger;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static circus.animal.Animal.AnimalNameComparator;
 
 public class Circus {
     private static Animal[] animals = {
@@ -47,37 +41,8 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        System.out.println("Number of animals in the array are: " + animals.length);
-//        animals[3] = new Elephant("Eli");
-        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
-
-        printAllAnimals(animalArrayList);
-
-        System.out.println("Size of our animal array list: " + animalArrayList.size());
-
-        Elephant strongOne = new Elephant("Strong one!");
-        animalArrayList.add(strongOne);
-        Duck andy = new Duck("Andy");
-        animalArrayList.add(andy);
-
-        System.out.println("Before sorting...");
-        printAllAnimals(animalArrayList);
-
-        System.out.println("Size of our animal array list: " + animalArrayList.size());
-
-        // For 0-Indexed - iterated like a string
-        System.out.println("Strong One is in position: " + (animalArrayList.indexOf(strongOne)+1));
-        animalArrayList.sort(AnimalNameComparator);
-        System.out.println("After sorting...");
-        printAllAnimals(animalArrayList);
-//        makeAnimalsTalk();
-//        System.out.println("Total value of animals " + calculateAssetValue(animals));
-//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
-    }
-
-    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
-        for(Animal a : animalArrayList) {
-            System.out.println(a);
-        }
+        makeAnimalsTalk();
+        System.out.println("Total value of animals " + calculateAssetValue(animals));
+        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
     }
 }
